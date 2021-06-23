39fd1fa087d5976c937daa5df02b07eb9796ada3
moong94 <hmg19940201@naver.com>
2021-06-23 22:44:15 +0900

    PS practice 20210623

---
+++ BOJ/Class4/11660.py
@@ -0,0 +1,16 @@
+import sys
+
+input = sys.stdin.readline
+n, m = map(int,input().split())
+
+matrix = [list(map(int,input().split())) for _ in range(n)]
+dp = [[0 for _ in range(n + 1)] for _ in range(n + 1)]
+
+for i in range(n):
+    for j in range(n):
+        dp[i + 1][j + 1] = dp[i][j + 1] + dp[i + 1][j] - dp[i][j] + matrix[i][j]
+
+for i in range(m):
+    x1,y1,x2,y2 = map(int,input().split())
+    answer = dp[x2][y2] - dp[x1 - 1][y2] - dp[x2][y1 - 1] + dp[x1 - 1][y1 - 1]
+    print(answer)
