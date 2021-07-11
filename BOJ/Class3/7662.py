133683784c56b8d9de51ccf12d614170a7ed1b61
moong94 <hmg19940201@naver.com>
2021-07-11 23:54:43 +0900

    PS practice 20210711

--- BOJ/Class3/7662.py
+++ BOJ/Class3/7662.py
@@ -1,45 +1,34 @@
 import heapq
 
+t = int(input())
 
-def sync(arr):
-    while arr and id[arr[0][1]] == 0:
-        heapq.heappop(arr)
+def test_case(k):
+    min_heap = []
+    max_heap = []
 
-
-T = int(input())
-for test_case in range(T):
-    max_arr = []
-    min_arr = []
-    id = [0] * 1000000
-    K = int(input())
-    count = 0
-    for i in range(K):
-        S, num = input().split()
-
-        if S == "I":
-            heapq.heappush(max_arr, (-1 * int(num), i))
-            heapq.heappush(min_arr, (int(num), i))
-            id[i] = 1
-
-        else:
-
-            if num == "1":
-                sync(max_arr)
-                if max_arr:
-                    id[max_arr[0][1]] = 0
-                    heapq.heappop(max_arr)
-
-            elif num == "-1":
-                sync(min_arr)
-                if min_arr:
-                    id[min_arr[0][1]] = 0
-                    heapq.heappop(min_arr)
-
-    sync(max_arr)
-    sync(min_arr)
-
-    if len(max_arr) == 0:
-        print("EMPTY")
+    arr = []
+    for _ in range(k):
+        oper, n = map(str,input().split())
+        n = int(n)
+        if oper == "D":
+            if arr:
+                arr.pop()
+                if n == -1:
+                    heapq.heappop(min_heap)
+                elif n == 1:
+                    heapq.heappop(max_heap)
+            else:
+                continue
+        if oper == "I":
+            arr.append(n)
+            heapq.heappush(min_heap, n)
+            heapq.heappush(max_heap,n * -1)
+    if arr:
+        print(heapq.heappop(max_heap) * -1, heapq.heappop(min_heap))
     else:
-        print(-1 * max_arr[0][0], end=" ")
-        print(min_arr[0][0])
+        print("EMPTY")
+
+
+for _ in range(t):
+    k = int(input())
+    test_case(k)
