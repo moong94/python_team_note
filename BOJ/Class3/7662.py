fe6f0f4f6c4d0289905c648b2ff5202a12e8ee74
moong94 <hmg19940201@naver.com>
2021-07-12 00:35:04 +0900

    PS practice 20210712

--- BOJ/Class3/7662.py
+++ BOJ/Class3/7662.py
@@ -1,30 +1,43 @@
 import heapq
+import sys
+
+input = sys.stdin.readline
 
 t = int(input())
 
 def test_case(k):
     min_heap = []
     max_heap = []
-
+    visited = [False] * 1000001
     arr = []
-    for _ in range(k):
+    for i in range(k):
         oper, n = map(str,input().split())
         n = int(n)
         if oper == "D":
-            if arr:
-                arr.pop()
-                if n == -1:
-                    heapq.heappop(min_heap)
-                elif n == 1:
+            if n == 1:
+                while max_heap and not visited[max_heap[0][1]]:
                     heapq.heappop(max_heap)
-            else:
-                continue
+                if max_heap:
+                    visited[max_heap[0][1]] = False
+                    heapq.heappop(max_heap)
+            elif n == -1:
+                while min_heap and not visited[min_heap[0][1]]:
+                    heapq.heappop(min_heap)
+                if min_heap:
+                    visited[min_heap[0][1]] = False
+                    heapq.heappop(min_heap)
         if oper == "I":
             arr.append(n)
-            heapq.heappush(min_heap, n)
-            heapq.heappush(max_heap,n * -1)
-    if arr:
-        print(heapq.heappop(max_heap) * -1, heapq.heappop(min_heap))
+            heapq.heappush(min_heap, (n, i))
+            heapq.heappush(max_heap, (n * -1, i))
+            visited[i] = True
+    while max_heap and not visited[max_heap[0][1]]:
+        heapq.heappop(max_heap)
+    while min_heap and not visited[min_heap[0][1]]:
+        heapq.heappop(min_heap)
+
+    if max_heap and min_heap:
+        print(max_heap[0][0] * -1, min_heap[0][0])
     else:
         print("EMPTY")
 
